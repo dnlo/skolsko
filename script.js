6a7092f55492216b0df8ed1f5892877d93d30291
dnlo <dnlo@gmx.com>
2016-08-05 12:20:36 +0200

    sa dugmetom

---
+++ script.js
@@ -0,0 +1,24 @@
+function gl() {
+    document.getElementById("gl").style.display = "block";
+}
+function dl() {
+    document.getElementById("dl").style.display = "block";
+}
+function mt() {
+    document.getElementById("mt").style.display = "block";
+}
+function mb() {
+    document.getElementById("mb").style.display = "block";
+}
+function dd() {
+    document.getElementById("dd").style.display = "block";
+}
+function bl() {
+    document.getElementById("bl").style.display = "block";
+}
+function bd() {
+    document.getElementById("bd").style.display = "block";
+}
+function x() {
+    document.getElementById("gl").style.display = "none";
+}
